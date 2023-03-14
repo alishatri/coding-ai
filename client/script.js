ea3d6dae2aee87536f0ca4e7c6474c6bf3fbe778
Team A <alishatri21@gmail.com>
2023-03-14 21:12:40 +0100

    api

--- client/script.js
+++ client/script.js
@@ -4,6 +4,8 @@ import user from "./assets/user.svg";
 const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat_container");
 
+const apiURL = "https://a-z-code.onrender.com";
+
 let loadInterval;
 
 function loader(element) {
@@ -67,7 +69,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch(apiURL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
